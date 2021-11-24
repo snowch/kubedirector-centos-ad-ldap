3638464b9774fbade79e3bb7e276a2e64cdadd52
Chris Snow <chsnow123@gmail.com>
2021-11-24 13:26:16 +0000

    Create auth.py

---
+++ appconfig/auth.py
@@ -0,0 +1,17 @@
+#!/bin/env python
+
+# Imports of standard Python packages.
+import sys
+import subprocess
+import os
+import os.path
+import errno
+import filecmp
+import platform
+import urlparse
+import ConfigParser
+import base64, json
+
+from configcli import ConfigCli
+
+print("test from auth.py")
